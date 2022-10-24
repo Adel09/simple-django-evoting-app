e8ca8b0ff9666fd79cf300d5c61e96ba4ce7b402
Adel09 <adelnehikhare@gmail.com>
2022-10-25 00:02:57 +0100

    final results

--- main/views.py
+++ main/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.models import User
 from django.db import IntegrityError
+from dashboard.models import Candidate
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/index.html')
+    candidates = Candidate.objects.all()
+    context = {
+            "candidates" : candidates
+        }
+    return render(request, 'main/index.html', context)
 
 def loginuser(request):
     if request.method == 'GET':
